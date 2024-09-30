6a421abf7c5cb36f43cfa105490f1971627c0b32
Artag/Chris <scristxyz@gmail.com>
2024-09-30 15:41:19 -0500

    act

---
+++ src/domain/interfaces/fourVariableDocument.ts
@@ -0,0 +1,46 @@
+export interface FourVariableDocument{
+    messaging_product: "whatsapp";
+    to: string;
+    type: "template";
+    template: Template;
+  }
+  
+   interface Template  {
+      name: string,
+      language: {
+        code: "es_MX",
+      },"components": [
+        {
+        type: "header",
+        "parameters": [
+          {
+            type: "document",
+            "document":{
+                "link": string    
+            }
+          }
+        ]
+      },
+      {
+        type: "body",
+        "parameters": [
+          {
+            type: "text",
+            text: string
+          },
+          {
+            type: "text",
+            text: string
+          },
+          {
+            type: "text",
+            text: string
+          },
+          {
+            type: "text",
+            text: string
+          }
+        ]
+      }
+    ]
+  };
