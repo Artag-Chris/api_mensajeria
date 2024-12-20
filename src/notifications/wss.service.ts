735b10afc99ddc0ab898a3d6da3bbce6ac984e66
Artag/Chris <scristxyz@gmail.com>
2024-12-20 16:21:05 -0500

    asct ws

--- src/notifications/wss.service.ts
+++ src/notifications/wss.service.ts
@@ -1,17 +1,25 @@
 import { Server } from 'http';
 import { WebSocket, WebSocketServer } from 'ws';
 import PrismaService from '../prisma/prisma.service';
-
+import logger from '../config/adapters/winstonAdapter';
 interface Options {
   server: Server;
   path?: string; // ws
 }
 
 export class WssService {
+
+   /**************************************************************************************************
+    clase refrenete a la coneccion por websocket se encarga de enviar mensajes a los clientes conectados
+    y de parsear la data que manda el bot de whatsapp.
+    aqui estan los cambios de whatsapp a websocket
+   ***************************************************************************************************/
+
   private static _instance: WssService;
   private wss: WebSocketServer;
 
   private constructor(options: Options) {
+    //si tuvieramos https se cambiara a wss
     const { server, path = '/ws' } = options;
 
     this.wss = new WebSocketServer({ server, path });
@@ -44,18 +52,18 @@ export class WssService {
       // Aquí van las notificaciones de websocket
       ws.on('message', (message: any) => {
         const messageString = message.toString('utf8');
-        console.log('mensaje recibido:', messageString);
         try {
           const data = JSON.parse(messageString);
+          console.log(`mensaje recibido: numero ${data.phone} ${data.name}: ${data.message}`);
           // Aquí puedes manejar el objeto JSON recibido
           this.sendMessage('broadcast', data);
           prismaService.onMessageReceived(data);
-        } catch (error) {
-          console.error('Invalid JSON received:', messageString);
+        } catch (error:any) {
+          logger.error('Error en mensaje enviado al websocket de la api', error?.message);
         }
       });
 
-      console.log('cliente conectado');
+      //console.log('cliente conectado');
       ws.on('close', () => console.log('Client disconnected'));
     });
   }
